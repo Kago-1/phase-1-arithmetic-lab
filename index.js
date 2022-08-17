713ef4ef5f0b433a88eb0f69acaa8f35dcbebc93
Stephen <stephenkago10@gmail.com>
2022-08-17 21:42:28 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+console.log (multiply);
+
+let random = Math.floor(Math.random() * 10 +1);
+console.log (random);
+
+let num3 = 22;
+let num4 = 9;
+
+const mod = num3 % num4;
+console.log (mod);
+
+
+let max = Math.max(0, 5, 10, 15, 20); //will return 20 as the highest number in the set
+console.log(max);
